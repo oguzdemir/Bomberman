b416c5a7aeb78961502cd010609e5d580c37518b
Anıl Sert <anil.sert9@gmail.com>
2016-04-24 18:18:59 +0300

    OverlapEngine class

--- Implementation/Bomberman/src/OverlapEngine.java
+++ Implementation/Bomberman/src/OverlapEngine.java
@@ -1,11 +1,22 @@
 /**
- * Created by od on 23.4.2016.
+ * Engine for checking collisions between GameObject's.
+ *
+ * Created by Anıl Sert on 24.4.2016.
  */
 public class OverlapEngine
 {
 
-    public boolean checkCollide(GameObject obj, GameObject obj2)
+    /**
+     * Check the collisions between two GameObject given as a parameter
+     * and give the result according to their positions.
+     *
+     * @param obj1 first object for checking collisions between.
+     * @param obj2 second object for checking collisions between.
+     * @return objects are colliding or not
+     */
+    public boolean checkCollide(GameObject obj1, GameObject obj2)
     {
-        return true;
+        return  obj1.getXPosition() == obj2.getXPosition() &&
+                obj1.getYPosition() == obj2.getYPosition();
     }
 }
