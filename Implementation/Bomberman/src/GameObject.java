cbcecd332c449d10ee344c9dd599389637cc584f
Anıl Sert <anil.sert9@gmail.com>
2016-04-24 17:44:34 +0300

    GameObject class

--- Implementation/Bomberman/src/GameObject.java
+++ Implementation/Bomberman/src/GameObject.java
@@ -1,7 +1,20 @@
 /**
- * Created by od on 23.4.2016.
+ * Created by Anıl Sert on 24.4.2016.
  */
 public abstract class GameObject
 {
+    // Local Variables
+    private int xPosition, yPosition;
 
+    /**
+     * The constructor of the GameObject class
+     *
+     * @param x x position of the created GameObject
+     * @param y y position of the created GameObject
+     */
+    public GameObject (int x, int y)
+    {
+        this.xPosition = x;
+        this.yPosition = y;
+    }
 }
