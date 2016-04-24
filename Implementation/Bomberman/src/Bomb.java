d71681813a45ecc9406b867a02914b2bc3c30963
Anıl Sert <anil.sert9@gmail.com>
2016-04-24 18:05:37 +0300

    Bomb class

--- Implementation/Bomberman/src/Bomb.java
+++ Implementation/Bomberman/src/Bomb.java
@@ -1,7 +1,33 @@
 /**
- * Created by od on 23.4.2016.
+ * Bomb class for the bomb objects created by the Bomberman.
+ *
+ * Created by Anıl Sert on 24.4.2016.
  */
 public class Bomb extends GameObject
 {
+    // Local Variables
+    private int timeLeft;
 
+    /**
+     *
+     * @param x x position of the created Bomb.
+     * @param y y position of the created Bomb.
+     */
+    public Bomb (int x, int y, int timeLeft)
+    {
+        super (x, y);
+        this.timeLeft = timeLeft;
+    }
+
+    /**
+     * The count down for the explosion of the Bomb.
+     *
+     * @return the bomb time is 0 or not.
+     */
+    public boolean countDown ()
+    {
+        timeLeft--;
+
+        return timeLeft <= 0;
+    }
 }
