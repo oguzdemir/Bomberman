fe64892fcaa6cb334fc7ffd1c8382d520e3e9f5f
Anıl Sert <anil.sert9@gmail.com>
2016-04-26 18:01:27 +0300

    Generate getter and setter methods for GameManager

--- Implementation/Bomberman/src/GameManager.java
+++ Implementation/Bomberman/src/GameManager.java
@@ -31,4 +31,110 @@ public class GameManager {
     {
 
     }
+
+    
+
+    public static GameManager getInstance() {
+        return instance;
+    }
+
+    public static void setInstance(GameManager instance) {
+        GameManager.instance = instance;
+    }
+
+    public int getCurrentLevel() {
+        return currentLevel;
+    }
+
+    public void setCurrentLevel(int currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+
+    public int getRemainingTime() {
+        return remainingTime;
+    }
+
+    public void setRemainingTime(int remainingTime) {
+        this.remainingTime = remainingTime;
+    }
+
+    public int getCurrentScore() {
+        return currentScore;
+    }
+
+    public void setCurrentScore(int currentScore) {
+        this.currentScore = currentScore;
+    }
+
+    public int getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(int gameState) {
+        this.gameState = gameState;
+    }
+
+    public int getSoundLevel() {
+        return soundLevel;
+    }
+
+    public void setSoundLevel(int soundLevel) {
+        this.soundLevel = soundLevel;
+    }
+
+    public int getMusicLevel() {
+        return musicLevel;
+    }
+
+    public void setMusicLevel(int musicLevel) {
+        this.musicLevel = musicLevel;
+    }
+
+    public String getMusicAdr() {
+        return musicAdr;
+    }
+
+    public void setMusicAdr(String musicAdr) {
+        this.musicAdr = musicAdr;
+    }
+
+    public String getHighScores() {
+        return highScores;
+    }
+
+    public void setHighScores(String highScores) {
+        this.highScores = highScores;
+    }
+
+    public GameEngine getgEngine() {
+        return gEngine;
+    }
+
+    public void setgEngine(GameEngine gEngine) {
+        this.gEngine = gEngine;
+    }
+
+    public MainFrame getFrame() {
+        return frame;
+    }
+
+    public void setFrame(MainFrame frame) {
+        this.frame = frame;
+    }
+
+    public FileManager getfManager() {
+        return fManager;
+    }
+
+    public void setfManager(FileManager fManager) {
+        this.fManager = fManager;
+    }
+
+    public SoundManager getsManager() {
+        return sManager;
+    }
+
+    public void setsManager(SoundManager sManager) {
+        this.sManager = sManager;
+    }
 }
