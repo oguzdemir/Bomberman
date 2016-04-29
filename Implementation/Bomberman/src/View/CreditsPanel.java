3900ba5061639e0e14927f9e3fbccd888937ae8b
Anıl Sert <anil.sert9@gmail.com>
2016-04-29 20:18:17 +0300

    Update CreditsPanel

--- Implementation/Bomberman/src/View/CreditsPanel.java
+++ Implementation/Bomberman/src/View/CreditsPanel.java
@@ -1,5 +1,8 @@
 package View;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by od on 28.4.2016.
  */
@@ -8,5 +11,23 @@ public class CreditsPanel extends InfoPanel
     public CreditsPanel()
     {
         super("Credits  ");
+
+        JTextArea text = new JTextArea("Oguz Demir - oguz.demir@ug.bilkent.edu.tr\n" +
+                "Kaan Kale - \n" +
+                "Anil Sert - anil.sert@ug.bilkent.edu.tr\n");
+
+
+        text.setSize(new Dimension(900,900));
+
+        text.setFont(customFont3);
+        text.setLineWrap(true);
+        text.setEditable(false);
+        text.setVisible(true);
+
+        text.setForeground(Color.BLUE);
+        text.setOpaque(false);
+
+        this.add(text);
+        text.setLocation(50,350);
     }
 }
